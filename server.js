26b6dbda5aaf2b5b1cfdbe06fbe6b4c07aff316a
Prasetyono Putra <prasetyonoptr@gmail.com>
2024-07-06 12:17:14 +0700

    Trainee Page

--- server.js
+++ server.js
@@ -2,25 +2,52 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
-let computer;
+const PORT = process.env.PORT || 8080;
+const HB_API_KEY = process.env.HB_API_KEY;
+const ACCESS_KEY = process.env.ACCESS_KEY;
+let computer = null;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use(express.json());
 
 app.get("/computer", async (req, res) => {
-    if (computer) {
-        res.send(computer);
-        return;
-    }
-    const resp = await axios.post(
-        "https://engine.hyperbeam.com/v0/vm",
-        {},
-        {
-            headers: { Authorization: `Bearer ${process.env.HB_API_KEY}` },
+    try {
+        if (computer) {
+            return res.send(computer);
         }
-    );
-    computer = resp.data;
-    res.send(computer);
+        const resp = await axios.post(
+            "https://engine.hyperbeam.com/v0/vm",
+            {},
+            {
+                headers: { Authorization: `Bearer ${HB_API_KEY}` },
+            }
+        );
+        computer = resp.data;
+        res.send(computer);
+    } catch (error) {
+        console.error("Error fetching computer data:", error);
+        res.status(500).send("Internal Server Error");
+    }
 });
 
-app.listen(process.env.PORT, () => console.log("Server start at http://localhost:" + process.env.PORT));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get("/mirror", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'mirror.html'));
+});
+
+app.use((err, req, res, next) => {
+    console.error("Unhandled Error:", err);
+    res.status(500).send("Something went wrong!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
